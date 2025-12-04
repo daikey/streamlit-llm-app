f9f1038dca8876de1a57e4e159c907d5cfafbad6
daikey <daiki.int@gmail.com>
2025-12-04 18:40:07 +0900

    fix app.py: import langchain_openai

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 import streamlit as st
-from langchain.openai import chatopenai
+from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 
